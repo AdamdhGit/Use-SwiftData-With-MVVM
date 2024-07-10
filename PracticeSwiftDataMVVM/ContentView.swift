4ba18c28bdf120eb51fb0418408477febcd98f5a
Adam Heidmann <adamdhm@yahoo.com>
2024-07-10 16:42:24 -0700

    Initial Commit

---
+++ PracticeSwiftDataMVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeSwiftDataMVVM
+//
+//  Created by Adam Heidmann on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeSwiftDataMVVM/PracticeSwiftDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftDataMVVMApp.swift
+//  PracticeSwiftDataMVVM
+//
+//  Created by Adam Heidmann on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
